8150d9dc71c97f805ec96c4ff248fc0e979f8201
monishaj <monijaya33@gmail.com>
2021-10-11 21:09:56 +0530

    second commit

---
+++ second.java
@@ -0,0 +1,17 @@
+package second;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte  number = 20  ;
+		short number2 = 150;
+		int number3 = 123456789;
+		long number4 =123456789L;
+		System.out.println(number3);
+		
+		
+
+	}
+
+}
