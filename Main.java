f3689d9e8c37bb5d6d2dfc7c131226b3be006e0a
monishaj <monijaya33@gmail.com>
2021-10-11 19:41:37 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package online;
+
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=x+y;
+
+      System.out.println("Sum of x+y = " + z);
+    }
+}
