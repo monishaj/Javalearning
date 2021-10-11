74bd36269bfaa5236a587a2de7a5bbafe4c940e2
monishaj <monijaya33@gmail.com>
2021-10-11 22:02:03 +0530

    third commit

---
+++ third.java
@@ -0,0 +1,11 @@
+package third;
+class Main{
+public static void main(String[] args){
+	int[] numbers = {10,11,12,13,15};
+	//number[0]=10;
+	//number[1]=20;
+	char name[]= {'a','b','c','d'};
+	System.out.println(numbers);
+	System.out.println(name);
+}
+}
